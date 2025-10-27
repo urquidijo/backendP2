7ebe9c7f79fdd1d0b268821c4b8684cf40fcd313
urquidijo <josueurquidicarrillo@gmail.com>
2025-10-27 13:18:27 -0400

    cors

--- backend/settings.py
+++ backend/settings.py
@@ -156,6 +156,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 CORS_ALLOWED_ORIGINS = [
     'http://localhost:5173',
     'http://127.0.0.1:5173',
+    'https://frontend-p2.vercel.app',
 ]
 
 CORS_ALLOW_CREDENTIALS = True
